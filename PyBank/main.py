7677305fa48b64ec0070570f2a60f2a577a2ae30
Marisela Castillo <marisela2castillo@gmail.com>
2021-01-17 20:11:30 -0600

    Import file

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,5 +1,22 @@
-# Call os Module
+# Import the os Module
 import os
-#import fie
+
+# Import Module for reading CSV files
 import csv
-path=os.path.join("Resources", "budget_data.csv")
+
+csvpath = os.path.join('PyBank', 'Resources', 'budget_data.csv')
+
+# Improved CSV file
+with open (csvpath) as csvfile:
+
+    #CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader (csvfile, delimiter = ',')
+    print (csvreader)
+
+    #Read the header row first
+    csv_header = next (csvreader)
+    print(f'CSV Header: {csv_header}')
+
+    #Read each row after the heading
+    for row in csvreader:
+        print (row)
