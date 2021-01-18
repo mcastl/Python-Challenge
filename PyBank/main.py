d7ba707b394992f08b832e38eef6c4926056e074
Marisela Castillo <marisela2castillo@gmail.com>
2021-01-17 20:29:51 -0600

    import data

--- PyBank/main.py
+++ PyBank/main.py
@@ -15,7 +15,8 @@ with open (csvpath) as csvfile:
 
     #Read the header row first
     csv_header = next (csvreader)
-    print(f'CSV Header: {csv_header}')
+    print(f'CSV Header: \n{csv_header}')
+    print('CSV contents:\n')
 
     #Read each row after the heading
     for row in csvreader:
