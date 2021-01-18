f71421054c144db6597ca83c9fa313d493826f8a
Marisela Castillo <marisela2castillo@gmail.com>
2021-01-17 22:44:40 -0600

    Compute average change

--- PyBank/main.py
+++ PyBank/main.py
@@ -18,6 +18,37 @@ with open (csvpath) as csvfile:
     print(f'CSV Header: \n{csv_header}')
     print('CSV contents:\n')
 
-    #Read each row after the heading
+    # Add columns for calculations
+    Months_count = []
+    Balance = []
+    Average_Change = []
+    Change = []
+
+    # Read each row of data after the header
     for row in csvreader:
-        print (row)
+    #Add new elements to the list
+        Months_count.append(row[0])
+        Balance.append(row[1])
+    
+    # Calculate the total number of months included in the dataset
+    print(len(Months_count))
+
+    # Calculate the net total amount of "Profit/Losses" over the entire period
+    Balance_int = map(int, Balance)
+    Total = (sum(Balance_int))
+    print (Total)
+
+    # Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
+    x = 0
+    for x in range(len(Balance) - 1):
+        New_value = int(Balance[x + 1])
+        Old_value = int(Balance[x])
+        Monthly_change = int(New_value - Old_value)
+        Change.append(Monthly_change)
+    Change_total = sum(Change)
+    Average_Change = Change_total / len(Change)
+    print(Average_Change)
+
+    # Calculate the greatest increase in profits (date and amount) over the entire period
+
+    # Calculate he greatest decrease in losses (date and amount) over the entire period  
